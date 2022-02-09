f80810cce3b4b10c0a1fab5a96b1bf68ed97b643
Tim Bachmann <t.bachmann@unibas.ch>
2022-02-09 10:50:00 +0100

    Created empty xCode Project

---
+++ CapVis-AR/CapVis-AR/CapVis_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapVis_ARApp.swift
+//  CapVis-AR
+//
+//  Created by Tim Bachmann on 09.02.22.
+//
+
+import SwiftUI
+
+@main
+struct CapVis_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapVis-AR/CapVis-AR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CapVis-AR
+//
+//  Created by Tim Bachmann on 09.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
